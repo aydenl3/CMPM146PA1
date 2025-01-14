a16df45a32349333f0749801338481024c06559d
aydenl3 <soloyeti41@gmail.com>
2025-01-13 21:02:25 -0800

    Implemented Edge-Drawing Function

--- P1/src/nm_pathfinder.py
+++ P1/src/nm_pathfinder.py
@@ -31,27 +31,28 @@ def find_path (source_point, destination_point, mesh):
 
     #simple search algorithm
     try:
-        path = BFS(start_end_boxes[0], start_end_boxes[1], mesh, source_point, destination_point)
+        path = BFS(start_end_boxes[0], start_end_boxes[1], mesh, source_point, destination_point,boxes)
     except: #only runs when invalid inputs(needs 2 boxes), or the other case. 
         print("No path!2")
 
     return path, boxes.keys()
 
-def BFS(start, goal, mesh, sources, goals):
+def BFS(start, goal, mesh, sources, goals,TheDict):
     queue = [start] 
     TheSet = set([])
-    TheDict = {} # {CHILD : PARENT}
+    #TheDict = {} # {CHILD : PARENT}
     while True:
         node = queue.pop()
         if(node == goal):
             print("SUCCESS")
             path = [node]
-            drawnpath = [goals, boxToCenterPoint(node)]
+            drawnpath = [goals, boxesToEdgePoint(goals,node)]
             while path[-1] != start:
                 parent = TheDict.get(path[-1])
                 #other = boxesToEdgePoint(parent, path[0])
                 path.append(parent)
-                drawnpath.append(boxToCenterPoint(parent))
+                #print(f"PATHPOINT:{drawnpath[-1]}")
+                drawnpath.append(boxesToEdgePoint(drawnpath[-1],parent))
                 if(len(path) > 500):
                     print("PATH TOO LONG")
                     break
@@ -73,12 +74,31 @@ def boxToCenterPoint(box): #Turns a box into a center point and returns it (x,y)
     pointtwo = (box[2] + box[3]) / 2 
     return [pointone, pointtwo]
 
-def boxesToEdgePoint(box, boxtwo): #Finds a range at the border of two points. Returns the average of the range (x,y).
-    pointone = max(box[0], boxtwo[0])
-    pointtwo = min(box[1], boxtwo[1])
-    pointthree = max(box[2], boxtwo[2])
-    pointfour = min(box[3], boxtwo[3])
-    return [(pointthree + pointfour)/ 2, (pointone + pointtwo) / 2] 
+def boxesToEdgePoint(point, box): #Finds a range at the border of two points. Returns the average of the range (x,y).
+    p1 = (box[0],box[2])
+    p2 = (box[1],box[2])
+    p3 = (box[0],box[3])
+    p4 = (box[1],box[3])
+    #print("1")
+    dist1 = euclideanDistance(point,p1)
+    dist2 = euclideanDistance(point,p2)
+    dist3 = euclideanDistance(point,p3)
+    dist4 = euclideanDistance(point,p4)
+    #print(min(euclideanDistance(point,p1),euclideanDistance(point,p2),euclideanDistance(point,p3),euclideanDistance(point,p4)))
+    if(dist1 <= dist2 and dist1 <= dist3 and dist1 <= dist4):
+        #print(dist1)
+        return p1
+    elif(dist2 <= dist1 and dist2 <= dist3 and dist2 <= dist4):
+        #print(dist2)
+        return p2
+    elif(dist3 <= dist1 and dist3 <= dist2 and dist3 <= dist4):
+        #print(dist3)
+        return p3
+    elif(dist4 <= dist1 and dist4 <= dist2 and dist4 <= dist3):
+        #print(dist4)
+        return p4
+    else:
+        print("ERO")
 
 def euclideanDistance(point, pointtwo): #finds the closest disance between two points. Returns an int.
     return [math.sqrt((point[0] + pointtwo[0])^2 + (point[1] + pointtwo[1])^2)]
