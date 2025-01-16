a8ec760768a27203a987422951f69df780155191
aydenl3 <soloyeti41@gmail.com>
2025-01-15 20:56:56 -0800

    Cleanup

--- P1/src/nm_pathfinder.py
+++ P1/src/nm_pathfinder.py
@@ -27,21 +27,35 @@ def find_path (source_point, destination_point, mesh):
         if (destination_point[0] < box[1] and destination_point[0] > box[0]) and (destination_point[1] < box[3] and destination_point[1] > box[2]):
             start_end_boxes.append(box)
 
-    print(start_end_boxes)
-
-    """
     #simple search algorithm
+    """
     try:
         path = BFS(start_end_boxes[0], start_end_boxes[1], mesh, source_point, destination_point,boxes)
     except: #only runs when invalid inputs(needs 2 boxes), or the other case. 
         print("No path!")
     """
 
-    #A* implemenation
+
+
+    #A* implemenation  
+    """
+    try:
+        path = AStarMonodirection(source_point, destination_point, mesh["adj"], start_end_boxes[0], start_end_boxes[1], boxes)
+    except:
+        print("No path!")
+    """
+        
+
+
+    #A* BiDirectional implemenation
     try:
         path = AStarBidirection(source_point, destination_point, mesh["adj"], start_end_boxes[0], start_end_boxes[1], boxes)
     except:
         print("No path!")
+
+
+
+
     return path, boxes.keys()
 
 def BFS(start, goal, mesh, sources, goals,TheDict):
@@ -50,7 +64,7 @@ def BFS(start, goal, mesh, sources, goals,TheDict):
     while True:
         node = queue.pop()
         if(node == goal):
-            print("SUCCESS")
+            print("SUCCESS: Goal Found")
             path = [node]
             drawnpath = [goals, boxesToEdgePoint(goals,node)]
             while path[-1] != start:
@@ -58,7 +72,7 @@ def BFS(start, goal, mesh, sources, goals,TheDict):
                 path.append(parent)
                 drawnpath.append(boxesToEdgePoint(drawnpath[-1],parent))
                 if(len(path) > 500):
-                    print("PATH TOO LONG")
+                    print("ERR: Path Too Long")
                     break
             drawnpath.append(sources)
             return drawnpath 
@@ -68,17 +82,15 @@ def BFS(start, goal, mesh, sources, goals,TheDict):
                 if(x not in TheSet):
                     queue.append(x)
                     TheDict.update({x:node})
-        if(not queue): #checks at the END of the loop if queue is empty.
+        if(not queue):
             break
-    print("No path!") #if no other paths exist, this runs after if(not queue)
-
 
 def boxToCenterPoint(box): #Turns a box into a center point and returns it (x,y). 
     pointone = (box[1] + box[0]) / 2
     pointtwo = (box[2] + box[3]) / 2 
     return (pointone, pointtwo)
 
-def boxesToEdgePoint(point, box): 
+def boxesToEdgePoint(point, box): #Returns the closest corner of a box to a given point
     p1 = (box[0],box[2])
     p2 = (box[1],box[2])
     p3 = (box[0],box[3])
@@ -96,7 +108,7 @@ def boxesToEdgePoint(point, box):
     elif(dist4 <= dist1 and dist4 <= dist2 and dist4 <= dist3):
         return p4
     else:
-        print("ERROR")
+        print("ERR: Edge Handling Logic")
 
 def euclideanDistance(point, pointtwo): #finds the closest disance between two points. Returns an int.
     return [math.sqrt((point[0] - pointtwo[0])**2 + (point[1] - pointtwo[1])**2)]
@@ -112,7 +124,7 @@ def AStarMonodirection(start, goal, adj, startbox, endbox, TheDict):
 
         # If we reach the end box
         if currentbox == endbox:
-            print("SUCCESS")
+            print("SUCCESS: Goal Found")
             return reconstruct_path(TheDict, startbox, endbox, start, goal)
 
         # Else explore neighbors
@@ -125,9 +137,6 @@ def AStarMonodirection(start, goal, adj, startbox, endbox, TheDict):
                 priority = newcost + euclideanDistance(boxToCenterPoint(neighbor), boxToCenterPoint(endbox))[0]
                 heappush(queue, (priority, neighbor))
                 TheDict[neighbor] = currentbox
-
-    # If no path is found
-    print("NO PATH FOUND")
     return []
 
 def reconstruct_path(TheDict, startbox, endbox, start, goal):
@@ -144,30 +153,22 @@ def reconstruct_path(TheDict, startbox, endbox, start, goal):
     path.append(start)
     return path
 
-
-def path_to_cell(cell, path):#does not work with our implementaion
-    print (f"{path} \n\n\n")
-    if(cell == []):
-        return []
-    return path_to_cell(path[cell], path) + [cell]
-
 def AStarBidirection(start, goal, adj, startbox, endbox, TheDict):
     TheDicttwo = TheDict.copy()
     TheDict.update({startbox: None}) 
     TheDicttwo.update({endbox: None})
     pathcost = {startbox: 1}
-    pathcostother = {endbox: 0}#this is one otherwise it breaks. I dont know why
+    pathcostother = {endbox: 0}
     queue = []
     heappush(queue, (0, startbox, "Destination"))
     heappush(queue, (0, endbox, "Start"))
 
     while queue:
         priority, currentbox, direction = heappop(queue)
-
+        # If we reach the end box
         if currentbox in TheDict.keys() and currentbox in TheDicttwo.keys():
+            print("SUCCESS: Goal Found")
             return reconstruct_path_two(TheDict, startbox, endbox, start, goal, TheDicttwo, currentbox)
-
-
         # Else explore neighbors
         for neighbor in adj[currentbox]:
             if(direction == "Destination"):
@@ -186,11 +187,6 @@ def AStarBidirection(start, goal, adj, startbox, endbox, TheDict):
                     priority = newcost + euclideanDistance(boxToCenterPoint(neighbor), boxToCenterPoint(startbox))[0]
                     heappush(queue, (priority, neighbor, "Start"))
                     TheDicttwo[neighbor] = currentbox
-            
-            
-
-    # If no path is found
-    print("NO PATH FOUND")
     return []
 
     
